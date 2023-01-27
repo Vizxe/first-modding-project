f845db2762c3ed97cc0a66fa1ff7dde2d7721cd4
Parsa Hemmati <parsahemmati20@gmail.com>
2023-01-27 11:05:55 -0500

    Added blocks
    Added the blocks "Fuck ore" "Hard fuck ore" "Fuck ore," and added smelting and breaking capabilities

--- src/main/java/net/parsa/firstmod/FirstMod.java
+++ src/main/java/net/parsa/firstmod/FirstMod.java
@@ -1,6 +1,7 @@
 package net.parsa.firstmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.parsa.firstmod.block.ModBlocks;
 import net.parsa.firstmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,5 +13,6 @@ public class FirstMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
--- src/main/java/net/parsa/firstmod/item/ModItems.java
+++ src/main/java/net/parsa/firstmod/item/ModItems.java
@@ -1,11 +1,7 @@
 package net.parsa.firstmod.item;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.fabricmc.fabric.impl.itemgroup.FabricItemGroup;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -14,12 +10,12 @@ import net.parsa.firstmod.FirstMod;
 public class ModItems {
 
     public static final Item RAW_FUCK = registerItem("raw_fuck",
-            new Item(new Item.Settings()), ModItemGroup.FUCK);
+            new Item(new Item.Settings()));
     public static final Item LUKEWARM_FUCK = registerItem("lukewarm_fuck",
-            new Item(new Item.Settings()), ModItemGroup.FUCK);
+            new Item(new Item.Settings()));
 
-    private static Item registerItem(String name, Item item, ItemGroup group) {
-        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+    private static Item registerItem(String name, Item item) {
+        ItemGroupEvents.modifyEntriesEvent(ModItemGroup.FUCK).register(entries -> entries.add(item));
         return Registry.register(Registries.ITEM, new Identifier(FirstMod.MOD_ID, name), item);
     }
 
